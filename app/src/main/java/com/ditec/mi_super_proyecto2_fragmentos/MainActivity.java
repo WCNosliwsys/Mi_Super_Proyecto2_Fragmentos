67d0f229ae550515c23b433f01187d9830b7f2fd
Wilson Callisaya <demarktec@gmail.com>
2020-08-21 21:34:24 -0500

    Initial commit

---
+++ app/src/main/java/com/ditec/mi_super_proyecto2_fragmentos/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ditec.mi_super_proyecto2_fragmentos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
